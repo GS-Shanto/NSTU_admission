22d6cfdd1bf7a1e063d7bb02d65e6e5afec04b84
GS-Shanto <54944767+GS-Shanto@users.noreply.github.com>
2019-12-21 17:06:08 +0600

    Add files via upload

---
+++ admissiona,b.c
@@ -0,0 +1,242 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int roll;
+     while(1){
+    scanf("%d",&roll);
+    if(roll>=10001&&roll<=10036){
+    printf("ACADEMIC BUILDING: 1\t  Ground floor  ROOM:101\n");
+    }
+   else if(roll>=10037&&roll<=10072){
+    printf("ACADEMIC BUILDING: 1\t  Ground floor  ROOM:102\n");
+    }
+    else if(roll>=10073&&roll<=10120){
+    printf("ACADEMIC BUILDING: 1\t Ground floor  ROOM:103\n");
+    }
+    else if(roll>=10121&&roll<=10159){
+    printf("ACADEMIC BUILDING: 1\t 1st floor  ROOM:201\n");
+    }
+    else if(roll>=10160&&roll<=10198){
+    printf("ACADEMIC BUILDING: 1\t 1st floor  ROOM:202\n");
+    }
+    else if(roll>=10199&&roll<=10237){
+    printf("ACADEMIC BUILDING: 1\t 1st floor  ROOM:203\n");
+    }
+    else if(roll>=10238&&roll<=10273){
+    printf("ACADEMIC BUILDING: 1\t 2nd floor  ROOM:301\n");
+    }
+     else if(roll>=10274&&roll<=10309){
+    printf("ACADEMIC BUILDING: 1\t 2nd floor  ROOM:302\n");
+    }
+     else if(roll>=10310&&roll<=10342){
+    printf("ACADEMIC BUILDING: 1\t 2nd floor  ROOM:303\n");
+    }
+     else if(roll>=10343&&roll<=10396){
+    printf("ACADEMIC BUILDING: 1\t 3nd floor  ROOM:401\n");
+    }
+     else if(roll>=10397&&roll<=10450){
+    printf("ACADEMIC BUILDING: 1\t 3nd floor  ROOM:402\n");
+    }
+     else if(roll>=10451&&roll<=10498){
+    printf("ACADEMIC BUILDING: 1\t 4th floor  ROOM:501\n");
+    }
+     else if(roll>=10499&&roll<=10546){
+    printf("ACADEMIC BUILDING: 1\t 4th floor  ROOM:502\n");
+    }
+     else if(roll>=10547&&roll<=10594){
+    printf("ACADEMIC BUILDING: 1\t 4th floor  ROOM:503\n");
+    }
+    else if(roll>=10595&&roll<=10644){
+    printf("ACADEMIC BUILDING: 2\t 1st floor  ROOM:201\n");
+    }
+    else if(roll>=10645&&roll<=10694){
+    printf("ACADEMIC BUILDING: 2\t 1st floor  ROOM:202\n");
+    }
+    else if(roll>=10695&&roll<=10744){
+    printf("ACADEMIC BUILDING: 2\t 1st floor  ROOM:203\n");
+    }
+     else if(roll>=10745&&roll<=10786){
+    printf("ACADEMIC BUILDING: 2\t 2nd floor  ROOM:301\n");
+    }
+     else if(roll>=10787&&roll<=10836){
+    printf("ACADEMIC BUILDING: 2\t 2nd floor  ROOM:302\n");
+    }
+         else if(roll>=10837&&roll<=10896){
+    printf("ACADEMIC BUILDING: 2\t 3rd floor  ROOM:401\n");
+    }
+         else if(roll>=10897&&roll<=10956){
+    printf("ACADEMIC BUILDING: 2\t 3rd floor  ROOM:402\n");
+    }
+         else if(roll>=10957&&roll<=11006){
+    printf("ACADEMIC BUILDING: 2\t 3rd floor  ROOM:403\n");
+    }
+         else if(roll>=11007&&roll<=11050){
+    printf("ACADEMIC BUILDING: 2\t 3rd floor  ROOM:404\n");
+    }
+        else if(roll>=11051&&roll<=11100){
+    printf("ACADEMIC BUILDING: 2\t 4th floor  ROOM:501\n");
+    }
+     else if(roll>=11101&&roll<=11139){
+    printf("ACADEMIC BUILDING: 2\t 4th floor  ROOM:502\n");
+    }
+     else if(roll>=11140&&roll<=11178){
+    printf("ACADEMIC BUILDING: 2\t 4th floor  ROOM:503\n");
+    }
+    else if(roll>=11179&&roll<=11206){
+    printf("ACADEMIC BUILDING: 2\t 4th floor  ROOM:504\n");
+    }
+    else if(roll>=11207&&roll<=11251){
+    printf("ACADEMIC BUILDING: 2\t 5th floor  ROOM:601\n");
+    }
+    else if(roll>=11252&&roll<=11293){
+    printf("ACADEMIC BUILDING: 2\t 5th floor  ROOM:602\n");
+    }
+    else if(roll>=11294&&roll<=11338){
+    printf("ACADEMIC BUILDING: 2\t 5th floor  ROOM:603\n");
+    }
+    else if(roll>=11339&&roll<=11386){
+    printf("ACADEMIC BUILDING: 2\t 5th floor  ROOM:604\n");
+    }
+     else if(roll>=11387&&roll<=11419){
+    printf("ACADEMIC BUILDING: 2\t 6th floor  ROOM:701\n");
+    }
+     else if(roll>=11420&&roll<=11458){
+    printf("ACADEMIC BUILDING: 2\t 6th floor  ROOM:702\n");
+    }
+     else if(roll>=11459&&roll<=11497){
+    printf("ACADEMIC BUILDING: 2\t 6th floor  ROOM:703\n");
+    }
+     else if(roll>=11498&&roll<=11536){
+    printf("ACADEMIC BUILDING: 2\t 7th floor  ROOM:801\n");
+    }
+     else if(roll>=11537&&roll<=11578){
+    printf("ACADEMIC BUILDING: 2\t 7th floor  ROOM:802\n");
+    }
+    else if(roll>=11579&&roll<=11614){
+    printf("ACADEMIC BUILDING: 2\t 8th floor  ROOM:901\n");
+    }
+    else if(roll>=11615&&roll<=11647){
+    printf("ACADEMIC BUILDING: 2\t 8th floor  ROOM:902\n");
+    }
+    else if(roll>=11648&&roll<=11689){
+    printf("ACADEMIC BUILDING: 2\t 9th floor  ROOM:1001\n");
+    }
+    else if(roll>=11690&&roll<=11731){
+    printf("ACADEMIC BUILDING: 2\t 9th floor  ROOM:1002\n");
+    }
+    else if(roll>=11732&&roll<=11773){
+    printf("ACADEMIC BUILDING: 2\t 9th floor  ROOM:1004\n");
+    }
+     else if(roll>=11774&&roll<=11797){
+    printf("AUDITORIUM\t  1st floor  ROOM:201\n");
+    }
+    else if(roll>=11798&&roll<=11857){
+    printf("AUDITORIUM\t  1st floor  ROOM:202\n");
+    }
+    else if(roll>=11858&&roll<=11907){
+    printf("AUDITORIUM\t  1st floor  ROOM:203\n");
+    }
+    else if(roll>=11908&&roll<=11937){
+    printf("AUDITORIUM\t  1st floor  ROOM:204\n");
+    }
+    else if(roll>=11938&&roll<=11977){
+    printf("AUDITORIUM\t  2nd floor  ROOM:301\n");
+    }
+    else if(roll>=11978&&roll<=12022){
+    printf("AUDITORIUM\t  2nd floor  ROOM:302\n");
+    }
+    else if(roll>=12023&&roll<=12067){
+    printf("AUDITORIUM\t  2nd floor  ROOM:303\n");
+    }
+    else if(roll>=95001&&roll<=95016){
+    printf("AUDITORIUM\t  3rd floor  ROOM:401\n");
+    }
+        else if(roll>=12068&&roll<=12117){
+    printf("AUDITORIUM\t  3rd floor  ROOM:402\n");
+    }
+        else if(roll>=12118&&roll<=12177){
+    printf("AUDITORIUM\t  3rd floor  ROOM:403\n");
+    }
+        else if(roll>=12178&&roll<=12233){
+    printf("AUDITORIUM\t  3rd floor  ROOM:404\n");
+    }
+        else if(roll>=12234&&roll<=12299){
+    printf("AUDITORIUM\t  3rd floor  ROOM:405\n");
+    }
+      else if(roll>=12300&&roll<=12359){
+    printf("AUDITORIUM\t  4th floor  ROOM:501\n");
+    }
+    else if(roll>=12360&&roll<=12419){
+    printf("AUDITORIUM\t  4th floor  ROOM:502\n");
+    }
+    else if(roll>=12420&&roll<=12479){
+    printf("AUDITORIUM\t  4th floor  ROOM:503\n");
+    }
+    else if(roll>=12480&&roll<=12539){
+    printf("AUDITORIUM\t  4th floor  ROOM:504\n");
+    }
+    else if(roll>=12540&&roll<=12599){
+    printf("AUDITORIUM\t  4th floor  ROOM:505\n");
+    }
+    else if(roll>=12600&&roll<=12655){
+    printf("AUDITORIUM\t  4th floor  ROOM:506\n");
+    }
+      else if(roll>=12656&&roll<=12727){
+    printf("LIBRARY BHABAN\t Ground floor  ROOM:101\n");
+    }
+    else if(roll>=12728&&roll<=12777){
+    printf("LIBRARY BHABAN\t 1st floor  ROOM:201\n");
+    }
+    else if(roll>=12778&&roll<=12825){
+    printf("LIBRARY BHABAN\t 1st floor  ROOM:202\n");
+    }
+    else if(roll>=12826&&roll<=12925){
+    printf("LIBRARY BHABAN\t 1st floor  ROOM:204\n");
+    }
+    else if(roll>=12926&&roll<=12973){
+    printf("LIBRARY BHABAN\t 1st floor  ROOM:205\n");
+    }
+    else if(roll>=12974&&roll<=13021){
+    printf("LIBRARY BHABAN\t 1st floor  ROOM:206\n");
+    }
+    else if(roll>=13022&&roll<=13061){
+    printf("LIBRARY BHABAN\t 2ND floor  ROOM:301\n");
+    }
+        else if(roll>=13062&&roll<=13181){
+    printf("LIBRARY BHABAN\t 3rd floor  ROOM:402\n");
+    }
+       else if(roll>=13182&&roll<=13256){
+    printf("LIBRARY BHABAN\t 3rd floor  ROOM:403\n");
+    }
+       else if(roll>=90001&&roll<=90165){
+    printf("LIBRARY BHABAN\t 3rd floor  ROOM:401\n");
+    }
+     else if(roll>=13257&&roll<=13306){
+    printf("ADMINISTRATIVE BHABAN\t 3rd floor  ROOM:401\n");
+    }
+    else if(roll>=13307&&roll<=13350){
+    printf("ADMINISTRATIVE BHABAN\t 3rd floor  ROOM:403\n");
+    }
+        else if(roll>=13351&&roll<=13380){
+    printf("ADMINISTRATIVE BHABAN\t 4TH floor  ROOM:501\n");
+    }
+    else if(roll>=13381&&roll<=13426){
+    printf("ADMINISTRATIVE BHABAN\t 4TH floor  ROOM:502\n");
+    }
+    else if(roll>=13427&&roll<=13470){
+    printf("ADMINISTRATIVE BHABAN\t 4TH floor  ROOM:503\n");
+    }
+    else if(roll>=13471&&roll<=13504){
+    printf("ADMINISTRATIVE BHABAN\t 4TH floor  ROOM:504\n");
+    }
+    else if(roll==0){
+        return 0;
+    }
+    else {
+        printf("Invalid Roll Number\n ");
+    }
+    }
+    getch();
+}
